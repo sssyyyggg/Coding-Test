40ae8f9a05af47fe78b974a901605cc9e106e546
SON Younggwan <162323298+sssyyyggg@users.noreply.github.com>
2026-06-11 21:42:56 +0900

    [level 2] Title: 프로세스, Time: 1.80 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -6,30 +6,30 @@ class Solution {
         int id;
         int priority;
         
-        Process(int id, int priority){
-            this.id = id;
-            this.priority = priority;
-        }
+         Process(int id, int priority) {
+             this.id = id;
+             this.priority = priority;
+         }
     }
     
     public int solution(int[] priorities, int location) {
         
-        Queue<Process> q = new LinkedList<>();
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        Queue<Process> q = new LinkedList<>();
+        
+        int answer = 0;
         
         for (int i = 0; i < priorities.length; i++){
             q.offer(new Process(i, priorities[i]));
             pq.offer(priorities[i]);
         }
         
-        int answer = 0;
-        
         while(!q.isEmpty()){
             Process now = q.poll();
             
             if(now.priority == pq.peek()){
-                answer++;
                 pq.poll();
+                answer++;
                 
                 if(now.id == location){
                     return answer;
@@ -38,6 +38,9 @@ class Solution {
                 q.offer(now);
             }
         }
-        return 0;
+        
+        return answer;
+        
+        
     }
 }
