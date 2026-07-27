ea3a9a7ae28b0e4722810c4c1e3e233af2cab4c1
SON Younggwan <162323298+sssyyyggg@users.noreply.github.com>
2026-07-27 22:27:48 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.03 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] intArr = new int[n];
+        for(int i = 0; i < n; i++){
+            intArr[i] = num_list[i];
+        }
+        
+        return intArr;
+    }
+}
